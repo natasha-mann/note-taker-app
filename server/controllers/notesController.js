43912db0c7dd75bd96b1b1d5352f279a641e8e2a
Suraj Verma <surajverma@rentalcars.com>
2021-06-01 18:22:30 +0100

    try catch controllers

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -1,62 +1,82 @@
-const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const moment = require("moment");
 
 const { readFromFile, writeToFile } = require("../db/database");
 
 const getNotes = (req, res) => {
-  const data = readFromFile("db");
+  try {
+    const data = readFromFile("db");
 
-  res.json(data);
+    res.json(data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 };
 
 const postNotes = (req, res) => {
-  const data = readFromFile("db");
+  try {
+    const data = readFromFile("db");
 
-  const { title, text } = req.body;
+    const { title, text } = req.body;
 
-  // Add ID to the note
-  const id = uuidv4();
+    // Add ID to the note
+    const id = uuidv4();
 
-  // Add timestamp to note
-  const dateTime = moment().format("DD/MM/YY, HH:mm:ss");
+    // Add timestamp to note
+    const dateTime = moment().format("DD/MM/YY, HH:mm:ss");
 
-  const newNote = { title, text, id, dateTime };
+    const newNote = { title, text, id, dateTime };
 
-  data.push(newNote);
+    writeToFile("db", [newNote, ...data]);
 
-  writeToFile("db", JSON.stringify(data));
-
-  res.json(newNote);
+    res.json(newNote);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 };
 
 const updateNotes = (req, res) => {
-  const data = readFromFile("db");
+  try {
+    const data = readFromFile("db");
 
-  const note = req.body;
+    const { title, text } = req.body;
 
-  const { id } = req.params;
+    const { id } = req.params;
 
-  const index = data.findIndex((each) => each.id === id);
+    const index = data.findIndex((each) => each.id === id);
 
-  data[index].title = note.title;
-  data[index].text = note.text;
-  data[index].id = note.id;
-  data[index].dateTime = moment().format("DD/MM/YY, HH:mm:ss");
+    // find -> {title, text}
+    // filter
+    // [findafteredit, ...filtered]
 
-  writeToFile("db", JSON.stringify(data));
+    data[index].title = title;
+    data[index].text = text;
+    data[index].dateTime = moment().format("DD/MM/YY, HH:mm:ss");
 
-  res.status(200).json({ message: "Note successfully updated." });
+    writeToFile("db", data);
+
+    res.status(200).json({ message: "Note successfully updated." });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 };
 
 const deleteNotes = (req, res) => {
-  const data = readFromFile("db");
-  const { id } = req.params;
-  const newData = data.filter((note) => note.id !== id);
+  try {
+    const data = readFromFile("db");
+    const { id } = req.params;
+    const newData = data.filter((note) => note.id !== id);
 
-  writeToFile("db", JSON.stringify(newData));
+    writeToFile("db", newData);
 
-  res.status(200).json({ message: "Note successfully deleted." });
+    res.status(200).json({ message: "Note successfully deleted." });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 };
 
 module.exports = { getNotes, postNotes, updateNotes, deleteNotes };
